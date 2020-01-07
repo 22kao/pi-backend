e63c78f9708d82fdaa86572669e9e95bc4fa44f2
Isaque Ribeiro <isaquebrb@gmail.com>
2020-01-07 14:04:12 -0300

    project's creation

---
+++ src/test/java/br/com/iftm/adsge/pibackend/PiBackendApplicationTests.java
@@ -0,0 +1,12 @@
+package br.com.iftm.adsge.pibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
