b0133d90ce64dca77d348c6fad6dd5423f73c668
Diego Henrique Silva <diegosilvadhs@gmail.com>
2020-01-19 14:15:05 -0300

    Concluindo User

--- src/main/java/br/com/iftm/adsge/pibackend/repositories/UserRepository.java
+++ src/main/java/br/com/iftm/adsge/pibackend/repositories/UserRepository.java
@@ -2,9 +2,8 @@ package br.com.iftm.adsge.pibackend.repositories;
 
 import br.com.iftm.adsge.pibackend.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
 
-@Component
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
