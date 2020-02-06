3cf84574f43094ef6194e09e53769500417bea15
Isaque Ribeiro <isaquebrb@gmail.com>
2020-02-06 14:07:47 -0300

    Swagger implentation

--- src/main/java/br/com/iftm/adsge/pibackend/controller/ModuleController.java
+++ src/main/java/br/com/iftm/adsge/pibackend/controller/ModuleController.java
@@ -1,9 +1,13 @@
 package br.com.iftm.adsge.pibackend.controller;
 
+import br.com.iftm.adsge.pibackend.model.dto.ModuleDto;
+import br.com.iftm.adsge.pibackend.model.dto.PhoneDto;
 import br.com.iftm.adsge.pibackend.service.ModuleService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,4 +15,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class ModuleController {
 
     private final ModuleService service;
+
+    @GetMapping
+    public ResponseEntity<List<ModuleDto>> findAll(){
+        return ResponseEntity.ok().body(service.findAll());
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ModuleDto> findById(@PathVariable Integer id){
+        return ResponseEntity.ok().body(service.findById(id));
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ModuleDto> update(@PathVariable Integer id, @RequestBody ModuleDto dto){
+        return ResponseEntity.ok(service.update(id, dto));
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<ModuleDto> delete(@PathVariable Integer id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
